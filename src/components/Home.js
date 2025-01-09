00afb2095726e879f8bd5402786dd0c45b15d201
josemle <lewisweeks988@gmail.com>
2025-01-09 06:23:10 -0800

    create home header component

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import { Box } from '@mui/material'
+import Button from '@mui/material/Button'
+import OutlinedInput from '@mui/material/TextField'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function Auction() {
+  const navigate = useNavigate()
+  const [address, setAddress] = useState('')
+
+  return (
+    <>
+      <Box sx={{ my: 2 }}>
+        <OutlinedInput
+          value={address}
+          onChange={(event) => setAddress(event.target.value)}
+          label="Address"
+          fullWidth
+        />
+      </Box>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate(`/auctions/${address}`)
+        }}
+      >
+        Go To Contract
+      </Button>
+    </>
+  )
+}
