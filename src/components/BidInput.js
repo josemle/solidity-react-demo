7e26872ec28d0caec17c77739060db86c881955e
josemle <lewisweeks988@gmail.com>
2025-01-09 06:23:53 -0800

    create auction, bidinput, notfound component

---
+++ src/components/BidInput.js
@@ -0,0 +1,46 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import OutlinedInput from '@mui/material/TextField'
+import React, { useState } from 'react'
+
+export default function BidInput(props) {
+  const [bidAmount, setBidAmount] = useState('')
+
+  const { contractState } = props
+
+  function isValidNumber(num) {
+    return !isNaN(num)
+  }
+
+  function canBeWinningBid() {
+    return (
+      Number(bidAmount) + contractState.accountBid > contractState.highestBid
+    )
+  }
+
+  const validBid = isValidNumber(bidAmount) && canBeWinningBid()
+
+  return (
+    <>
+      <Box sx={{ my: 2 }}>
+        <OutlinedInput
+          value={bidAmount}
+          onChange={(event) => setBidAmount(event.target.value)}
+          label="Amount"
+          fullWidth
+          error={bidAmount !== '' && !validBid}
+        />
+      </Box>
+
+      <Button
+        variant="contained"
+        onClick={() => {
+          alert('TODO: Implement bidding')
+        }}
+        disabled={bidAmount === '' || !validBid}
+      >
+        Bid
+      </Button>
+    </>
+  )
+}
