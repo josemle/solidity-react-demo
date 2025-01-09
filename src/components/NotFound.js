e27d7eab5fe493c0afbbc26cd5f38b6a2ca89a8e
josemle <lewisweeks988@gmail.com>
2025-01-09 06:23:53 -0800

    create auction, bidinput, notfound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import Paper from '@mui/material/Paper'
+import Alert from '@mui/material/Alert'
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <Paper sx={{ p: 2, margin: 'auto', flexGrow: 1 }}>
+      <Alert severity="error">Page not found</Alert>
+    </Paper>
+  )
+}
