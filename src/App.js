1e19ab4a9c97a9095ba0d3dd5f3656c707200016
josemle <lewisweeks988@gmail.com>
2025-01-09 06:21:45 -0800

    app.js edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
 
-function App() {
+import Header from './components/Header'
+import Home from './components/Home'
+import Auction from './components/Auction'
+import NotFound from './components/NotFound'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+
+// Configure CSS
+const cache = createCache({
+  key: 'css',
+  prepend: true
+})
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#FAFAFA'
+    }
+  }
+})
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <CacheProvider value={cache}>
+        <div className="App">
+          <CssBaseline />
+          <Header />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/auctions/:address" element={<Auction />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Layout>
+        </div>
+      </CacheProvider>
+    </ThemeProvider>
+  )
 }
-
-export default App;
